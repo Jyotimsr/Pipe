296e88d7b7efcb5fbf3fb61526a55f4e8039d78c
unknown <prachimsr1998@gmail.com>
2024-02-12 12:53:03 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipe';
+  currentDate: Date = new  Date();
+  x:string = 'jyoti mishra';
+  y:string = 'prachi msr';
+  a=0.567;
+
+  b:{[s:string ]:number}={
+    b:1,c:2,d:3
+  };
+
+  students:any={
+    ID:'stu1',
+    Name:"JYOTI MISHRA",
+    DOB:23,
+    Gender:"female",
+    Fees:2000
+  }
+
 }
